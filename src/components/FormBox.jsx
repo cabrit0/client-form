b8676e629ce0ff2c3ca69541ea4704b8bad159c8
cabrit0 <cabrit0o.dev@gmail.com>
2022-10-15 10:29:54 +0100

    first_form-academia

---
+++ src/components/FormBox.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  Box,
+  Button,
+  Input,
+  FormControl,
+  FormLabel,
+  Textarea,
+} from "@chakra-ui/react";
+
+const FormBox = () => {
+  return (
+    <Box margin="auto" p={5} minW={280} maxW={400}>
+      <FormControl>
+        <FormLabel>Nome</FormLabel>
+        <Input
+          p={2}
+          mb={3}
+          _hover="#f5c451"
+          borderColor=""
+          focusBorderColor="#f5c451"
+          placeholder=""
+          _placeholder={{ opacity: 0.7, color: "#e9ecef" }}
+        />
+        <FormLabel>Email</FormLabel>
+        <Input
+          p={2}
+          mb={3}
+          _hover="#f5c451"
+          borderColor=""
+          focusBorderColor="#f1bd42"
+          placeholder="exemplo@email.net"
+          _placeholder={{ opacity: 0.7, color: "#e9ecef" }}
+        />
+        <FormLabel>Telemovel</FormLabel>
+        <Input
+          type="number"
+          p={2}
+          mb={3}
+          _hover="#f5c451"
+          borderColor=""
+          focusBorderColor="#f5c451"
+          placeholder="+351"
+          _placeholder={{ opacity: 0.7, color: "#e9ecef" }}
+        />
+        <FormLabel>Menssagem</FormLabel>
+        <Textarea
+          p={2}
+          mb={3}
+          _hover="#f5c451"
+          borderColor=""
+          focusBorderColor="#f5c451"
+          placeholder="Fala-nos um pouco de ti, e sobre os teus objetivos..."
+          _placeholder={{ opacity: 0.7, color: "#e9ecef" }}
+        />
+        <Button
+          colorScheme="#f5c451"
+          opacity=".7"
+          variant="outline"
+          _hover={{ opacity: 1 }}
+        >
+          Enviar
+        </Button>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default FormBox;
